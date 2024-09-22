dd935a305fb6613a1ddbd98a5de3f32b31a41015
vivek-dodiya <vivekdodiya1510@gmail.com>
2024-09-22 14:39:12 +0530

    commit-3

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -35,7 +35,12 @@ export default {
     },
     methods: {
         login() {
+            if(this.form.email === '' && this.form.password === '' ){
+                alert("all field are mendetory ");
+                return
+            }
             if (this.Login) {
+                console.log(this.form.email, this.form.password)
                 this.router.push('/dashboard');
             }
         }
